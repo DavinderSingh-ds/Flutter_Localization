ad1a405bd9b6fa805ffe268abb9c056020c58766
Davinder Singh <79924693+DavinderSingh-ds@users.noreply.github.com>
2022-03-04 22:20:08 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:localization_arb_example/l10n/l10n.dart';
+import 'package:localization_arb_example/page/localization_app_page.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:localization_arb_example/provider/locale_provider.dart';
+import 'package:provider/provider.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'Localization';
+
+  @override
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => LocaleProvider(),
+        builder: (context, child) {
+          final provider = Provider.of<LocaleProvider>(context);
+
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: title,
+            theme: ThemeData(
+              scaffoldBackgroundColor: Color.fromARGB(255, 53, 135, 243),
+              primaryColor: Colors.amber,
+            ),
+            locale: provider.locale,
+            supportedLocales: L10n.all,
+            localizationsDelegates: [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+            ],
+            home: MainPage(),
+          );
+        },
+      );
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int index = 0;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: LocalizationAppPage(),
+      );
+}
